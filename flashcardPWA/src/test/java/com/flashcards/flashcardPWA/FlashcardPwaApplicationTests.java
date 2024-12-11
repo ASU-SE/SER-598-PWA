b1152dd54e82feed8b8e1cb57a263d771bac97dd
arungangula <arungangula93@gmail.com>
2024-12-10 23:41:31 -0700

    spring boot intialized

---
+++ flashcardPWA/src/test/java/com/flashcards/flashcardPWA/FlashcardPwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcards.flashcardPWA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardPwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
